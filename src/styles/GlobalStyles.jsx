1424d4eef1d5a22b54a8efcd422211dbae7cec58
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-03-15 11:55:23 +0900

    style : 전역 스타일 적용

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+*,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  .a11y-hidden {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    margin: -1px;
+    clip-path: polygon(0 0, 0 0, 0 0);
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 50px;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ol,ul,li {
+    list-style: none;
+  }
+
+  input, button {
+    border: none;
+    outline: none;
+  }
+  
+  button {
+    cursor: pointer;
+  }    
+`;
+
+export default GlobalStyles;
