c389070b6af2de1d47aa5eb5aa086f0bbc11f2d5
chanyDev <chanyd.dev@gmail.com>
2022-03-24 18:30:27 +0900

    feat : 현재 디바이스 위치 기반의 근처 모임 요청 기능 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
@@ -7,6 +8,23 @@ import Banner from 'components/main/Banner';
 import MainContent from 'components/main/MainContent';
 
 const MainPage = () => {
+  useEffect(() => {
+    // 내 주변 모임 요청
+    const searchAroundMeeting = async (lon, lat) => {
+      const res = await axios.get(
+        `http://localhost:4000/api/meeting/search?lon=${lon}&lat=${lat}`,
+      );
+
+      console.log(res);
+    };
+
+    // 현재 디바이스의 경도, 위도를 가져옴
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { longitude: lon, latitude: lat } = position.coords;
+      searchAroundMeeting(lon, lat);
+    });
+  }, []);
+
   return (
     <>
       <Header />
