8d15026a39f5d471b37a71a9e8e3e46e6822451a
chanyDev <chanyd.dev@gmail.com>
2022-03-25 14:52:52 +0900

    refactor : 컴포넌트 이름 수정 및 디렉토리 위치 변경

--- src/pages/RegistPage.jsx
+++ src/pages/RegistPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import UserTemplate from 'components/common/UserTemplate';
+import RegistTemplate from 'components/regist/RegistTemplate';
 import RegistForm from 'components/regist/RegistForm';
 import Responsive from 'components/common/Responsive';
 
 const RegistPage = () => {
   return (
     <Responsive>
-      <UserTemplate>
+      <RegistTemplate>
         <RegistForm />
-      </UserTemplate>
+      </RegistTemplate>
     </Responsive>
   );
 };
