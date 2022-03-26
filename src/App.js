1fc38104a6b33a41e5e6541ccfc64ee20dc0d721
chanyDev <chanyd.dev@gmail.com>
2022-03-26 16:42:46 +0900

    refactor : 디렉토리 구조 변경으로 회원가입 페이지 라우터 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import KakaoPage from 'pages/KakaoPage';
-import RegistPage from 'pages/RegistPage';
 import MainPage from 'pages/MainPage';
 
 import RedirectPage from 'pages/RedirectPage';
@@ -12,7 +11,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/oauth/kakao" element={<KakaoPage />} />
-        <Route path="/regist" element={<RegistPage />} />
         <Route path="/redirect" element={<RedirectPage />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
