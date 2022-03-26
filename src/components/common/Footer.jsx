17de0d4a7467dca6a47d1bb27631a9fade6b7c69
chanyDev <chanyd.dev@gmail.com>
2022-03-26 17:39:04 +0900

    style : 푸터 레이아웃 수정

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,26 +4,51 @@ import { GithubOutlined } from '@ant-design/icons';
 
 const Footer = () => {
   return (
-    <FooterWrapper>
-      <p>Copyright ⓒ2022 LemonAlcohol. All rights reserved.</p>
-      <div>
-        <a href={'https://github.com/LemonAlcohol'}>
-          <GithubOutlined className="git-logo" />
-        </a>
-      </div>
-    </FooterWrapper>
+    <FooterOuter>
+      <FooterInner>
+        <p>Copyright ⓒ2022 LemonAlcohol. All rights reserved.</p>
+        <div className="github-wrapper">
+          <p>Github</p>
+          <a href={'https://github.com/LemonAlcohol'}>
+            <GithubOutlined className="git-logo" />
+          </a>
+        </div>
+      </FooterInner>
+    </FooterOuter>
   );
 };
+const FooterOuter = styled.div`
+  left: 0;
+  right: 0;
+  background-color: #fff;
+`;
 
-const FooterWrapper = styled.footer`
+const FooterInner = styled.footer`
   width: 100%;
-  padding: 2rem;
-  font-size: 2rem;
+  padding: 2rem 2rem;
+  margin: 0 auto;
+  font-size: 1.5rem;
   background-color: #fff;
   text-align: center;
+  align-items: center;
+  justify-content: space-between;
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
+
+  @media screen and (min-width: 768px) {
+    max-width: 1024px;
+    padding: 2rem 5rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    max-width: 1280px;
+    padding: 2rem 6rem;
+  }
+
+  .github-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
   .git-logo {
     font-size: 3rem;
   }
