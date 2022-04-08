ae667003aec7dfe6b32f4e20de713707ff9b8d96
chanyDev <chanyd.dev@gmail.com>
2022-04-08 18:05:22 +0900

    style : 모임 작성 페이지 레이아웃 일부 변경

--- src/pages/MeetingWritePage.jsx
+++ src/pages/MeetingWritePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import Header from 'components/common/Header';
 import Spacer from 'components/common/Spacer';
 import Outer from 'components/common/Outer';
 import Responsive from 'components/common/Responsive';
 import MeetingTemplate from 'components/meeting/MeetingTemplate';
-import MeetingEditor from 'components/meeting/MeetingEditor';
-import MeetingOption from 'components/meeting/MeetingOption';
+import LeftEditor from 'components/meeting/LeftEditor';
+import RightEditor from 'components/meeting/RightEditor';
+import MeetingWriteButton from 'components/meeting/MeetingWriteButton';
 
 const MeetingWritePage = () => {
   return (
@@ -14,9 +16,10 @@ const MeetingWritePage = () => {
       <Spacer />
       <Responsive>
         <MeetingTemplate>
-          <MeetingEditor />
-          <MeetingOption />
+          <LeftEditor />
+          <RightEditor />
         </MeetingTemplate>
+        <MeetingWriteButton />
       </Responsive>
     </Outer>
   );
