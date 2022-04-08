7478f4187446f3cf705b75250424ee7d93a6c335
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-08 19:07:49 +0900

    refactor : 게시물 가져오기 API 쿼리 추가

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -5,20 +5,37 @@ import styled from 'styled-components';
 import * as boardAPI from 'api/board';
 import BoardItem from 'components/board/BoardItem';
 import Pagination from './Pagination';
+import { useSearchParams } from 'react-router-dom';
 
 const BoardList = () => {
-  const { data } = useQuery(['boards'], boardAPI.getBoards, {
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
-  });
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = {
+    sort: searchParams.get('sort'),
+    page: searchParams.get('page'),
+  };
+
+  const { data, isLoading } = useQuery(
+    ['boards', query],
+    () => boardAPI.getBoards(query),
+    {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+      keepPreviousData: true,
+    },
+  );
 
   return (
     <BoardListWrapper>
+      {isLoading && '로딩'}
       {data?.boards &&
         data.boards.map((board) => (
           <BoardItem key={board._id} boardInfo={board} />
         ))}
-      <Pagination />
+      <Pagination
+        searchParams={searchParams}
+        setSearchParams={setSearchParams}
+      />
     </BoardListWrapper>
   );
 };
@@ -26,7 +43,7 @@ const BoardList = () => {
 const BoardListWrapper = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 0.5rem;
 `;
 
 export default BoardList;
