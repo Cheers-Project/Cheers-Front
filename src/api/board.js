3afd08d723392c7f974d7db4d23a917c42915ef1
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-11 18:26:40 +0900

    refactor : 게시물 작성 서버상태 관리

--- src/api/board.js
+++ src/api/board.js
@@ -4,15 +4,16 @@ export const getBoards = (payload) => {
   return client.get('/board', { params: payload }).then((res) => res.data);
 };
 
-export const write = async (payload) => {
+export const write = (payload) => {
   const accessToken = localStorage.getItem('accessToken');
 
-  const res = await client.post('/board', payload, {
-    headers: {
-      Authorization: accessToken,
-    },
-  });
-  return res;
+  return client
+    .post('/board', payload, {
+      headers: {
+        Authorization: accessToken,
+      },
+    })
+    .then((res) => res.data);
 };
 
 export const uploadImg = async (payload) => {
