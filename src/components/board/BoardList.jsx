cd0bb935c297aac2acedac26649840bd98a05de1
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-11 18:27:28 +0900

    feat : 게시물 상세페이지 라우팅 이벤트 위임

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import * as boardAPI from 'api/board';
 import BoardItem from 'components/board/BoardItem';
 import Pagination from 'components/board/Pagination';
 
 const BoardList = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const query = {
@@ -24,14 +25,22 @@ const BoardList = () => {
     },
   );
 
+  const handleRouter = (e) => {
+    if (e.target.id) {
+      console.log(e.target);
+      navigate(`/board/${e.target.id}`);
+    }
+  };
+
   return (
-    <BoardListWrapper>
+    <BoardListOuter>
       {isLoading && '로딩'}
       {isError && '에러'}
-      {data?.boards &&
-        data.boards.map((board) => (
-          <BoardItem key={board._id} boardInfo={board} />
+      <BoardListWrapper onClick={handleRouter}>
+        {data?.boards.map((board) => (
+          <BoardItem key={board._id} boardInfo={board} className="board-item" />
         ))}
+      </BoardListWrapper>
       {data?.maxPage && (
         <Pagination
           maxPage={data.maxPage}
@@ -40,9 +49,14 @@ const BoardList = () => {
           setSearchParams={setSearchParams}
         />
       )}
-    </BoardListWrapper>
+    </BoardListOuter>
   );
 };
+
+const BoardListOuter = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
 const BoardListWrapper = styled.ul`
   display: flex;
