d1dd676888b6b1c927c660d5e098380099865995
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-11 18:54:09 +0900

    feat : 게시물 상세 정보 API 생성

--- src/api/board.js
+++ src/api/board.js
@@ -4,6 +4,10 @@ export const getBoards = (payload) => {
   return client.get('/board', { params: payload }).then((res) => res.data);
 };
 
+export const getBoradById = (id) => {
+  return client.get(`/board/${id}`).then((res) => res.data);
+};
+
 export const write = (payload) => {
   const accessToken = localStorage.getItem('accessToken');
 
