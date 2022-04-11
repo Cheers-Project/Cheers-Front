546e666f3992c98ca290e79fb8cfd9476e14f0f6
chanyDev <chanyd.dev@gmail.com>
2022-04-11 18:52:02 +0900

    refactor : API 요청 로직 슬라이더로 이동

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 import Responsive from 'components/common/Responsive';
 import Banner from 'components/main/Banner';
 import MainContent from 'components/main/MainContent';
-import * as meetAPI from 'api/meet';
 
 const MainPage = () => {
-  useEffect(() => {
-    // 현재 디바이스의 경도, 위도를 가져옴
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { longitude: lon, latitude: lat } = position.coords;
-      meetAPI.searchAroundMeeting(lon, lat);
-    });
-  }, []);
-
   return (
     <>
       <Header />
