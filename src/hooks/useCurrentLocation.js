549ddc85c98faaef653c18dcef8adf8a2eeedde0
chanyDev <chanyd.dev@gmail.com>
2022-04-18 14:52:59 +0900

    refactor : 성공, 실패 핸들러 분리 및 에러 상태 추가

--- src/hooks/useCurrentLocation.js
+++ src/hooks/useCurrentLocation.js
@@ -2,16 +2,23 @@ import { useState, useEffect } from 'react';
 
 const useCurrentLocation = () => {
   const [location, setLocation] = useState();
+  const [error, setError] = useState();
+
+  const handleSuccess = (position) => {
+    const { longitude: lon, latitude: lat } = position.coords;
+    setLocation({ lon, lat });
+  };
+
+  const handleFailure = (error) => {
+    setError(error);
+  };
 
   useEffect(() => {
     // 현재 디바이스의 경도, 위도를 가져옴
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { longitude: lon, latitude: lat } = position.coords;
-      setLocation({ lon, lat });
-    });
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleFailure);
   }, []);
 
-  return location;
+  return { location, error };
 };
 
 export default useCurrentLocation;
