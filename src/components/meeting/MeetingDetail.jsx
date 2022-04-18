05ea74ab3b66d9a4757470873e00c0629f8c32fc
chanyDev <chanyd.dev@gmail.com>
2022-04-18 17:46:43 +0900

    feat : 모임 조회수 증가 구현

--- src/components/meeting/MeetingDetail.jsx
+++ src/components/meeting/MeetingDetail.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+
+import * as meetingAPI from 'api/meeting';
 
 const MeetingDetail = () => {
-  return <div>MeetingDetail</div>;
+  const { id } = useParams();
+
+  const { data: meetingInfo } = useQuery(
+    ['meeting', id],
+    meetingAPI.fetchMeetingDetail,
+    {
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return <div>{meetingInfo?.meeting.view}</div>;
 };
 
 export default MeetingDetail;
--- src/pages/MeetingDetailPage.jsx
+++ src/pages/MeetingDetailPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+
 import Header from 'components/common/Header';
 import Spacer from 'components/common/Spacer';
+import Outer from 'components/common/Outer';
+import Responsive from 'components/common/Responsive';
+import MeetingDetail from 'components/meeting/MeetingDetail';
 
 const MeetingDetailPage = () => {
   return (
-    <>
+    <Outer>
       <Header />
       <Spacer />
-    </>
+      <Responsive>
+        <MeetingDetail />
+      </Responsive>
+    </Outer>
   );
 };
 
