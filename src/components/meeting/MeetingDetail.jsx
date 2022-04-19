96ee792c75074003e79a30609da8f4bf94d532ea
chanyDev <chanyd.dev@gmail.com>
2022-04-19 23:35:34 +0900

    feat : 작성자에게만 수정, 삭제 버튼 활성화

--- src/components/meeting/MeetingDetail.jsx
+++ src/components/meeting/MeetingDetail.jsx
@@ -3,11 +3,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import StyledButton from 'components/common/StyledButton';
 import useMeetingQuery from 'hooks/useMeetingQuery';
+import useOwnedQuery from 'hooks/useOwnedQuery';
 
 const MeetingDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const meetingInfo = useMeetingQuery();
+  const userId = useOwnedQuery();
 
   const handleNavigate = () => {
     navigate(`/meeting/write/${id}`);
@@ -19,10 +21,14 @@ const MeetingDetail = () => {
         {meetingInfo?.view}
       </p>
       <div style={{ display: 'flex', gap: '1rem' }}>
-        <StyledButton onClick={handleNavigate} cherry>
-          수정
-        </StyledButton>
-        <StyledButton>삭제</StyledButton>
+        {meetingInfo?.writer._id === userId && (
+          <>
+            <StyledButton onClick={handleNavigate} cherry>
+              수정
+            </StyledButton>
+            <StyledButton>삭제</StyledButton>
+          </>
+        )}
       </div>
     </div>
   );
