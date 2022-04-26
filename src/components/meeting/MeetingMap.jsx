aa44046f4ed105d8ab96d44205e7a313e8262da5
chanyDev <chanyd.dev@gmail.com>
2022-04-26 18:09:07 +0900

    refactor : 사용하지 않는 함수 제거

--- src/components/meeting/MeetingMap.jsx
+++ src/components/meeting/MeetingMap.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
