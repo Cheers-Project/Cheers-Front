6662758d26ec4ccac31edc86ba4b87cf68521954
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-27 18:10:53 +0900

    style : 반응형 레이아웃 수정

--- src/components/board/BoardNav.jsx
+++ src/components/board/BoardNav.jsx
@@ -10,24 +10,30 @@ const BoardNav = () => {
   return (
     <BoardNavWrapper>
       <NavList>
-        <Link
-          to={`/board?sort=recent&page=1`}
-          className={sort === 'recent' ? 'nav-item active' : 'recent nav-item'}
-        >
-          최신순
-        </Link>
-        <Link
-          to={`/board?sort=view&page=1`}
-          className={sort === 'view' ? 'nav-item active' : 'view nav-item'}
-        >
-          조회순
-        </Link>
-        <Link
-          to={`/board?sort=like&page=1`}
-          className={sort === 'like' ? 'nav-item active' : 'like nav-item'}
-        >
-          인기순
-        </Link>
+        <li className="nav-item">
+          <Link
+            to={`/board?sort=recent&page=1`}
+            className={sort === 'recent' ? 'nav-link active' : ' nav-link'}
+          >
+            최신순
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to={`/board?sort=view&page=1`}
+            className={sort === 'view' ? 'nav-link active' : 'nav-link'}
+          >
+            조회순
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to={`/board?sort=like&page=1`}
+            className={sort === 'like' ? 'nav-item active' : 'nav-link'}
+          >
+            인기순
+          </Link>
+        </li>
       </NavList>
     </BoardNavWrapper>
   );
@@ -36,16 +42,22 @@ const BoardNav = () => {
 const BoardNavWrapper = styled.div`
   display: flex;
   padding: 3rem 0;
+  margin-left: 1rem;
 `;
 
 const NavList = styled.ul`
   display: flex;
-  align-items: center;
-  gap: 2rem;
-
+  gap: 1rem;
+  @media screen and (min-width: 768px) {
+    gap: 2rem;
+  }
   .nav-item {
-    font-size: ${({ theme }) => theme.fontSize.md};
-    padding: 1rem;
+    display: flex;
+    align-items: center;
+  }
+  .nav-link {
+    font-size: ${({ theme }) => theme.fontSize.sm};
+    padding: 0.8rem;
     background-color: ${({ theme }) => theme.color.white};
     border-radius: 2rem;
     transition: 0.2s;
@@ -53,6 +65,10 @@ const NavList = styled.ul`
       border-radius: 2rem;
       color: ${({ theme }) => theme.color.white};
       background-color: ${({ theme }) => theme.color.lightCherry};
+    }
+    @media screen and (min-width: 768px) {
+      font-size: ${({ theme }) => theme.fontSize.md};
+      padding: 1rem;
     }
   }
   .active {
