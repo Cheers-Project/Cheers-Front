9f2381919a18714e444035e5d793eee9576ea775
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-05-03 18:05:33 +0900

    refactor : 카카오 REDIRECT URI 수정

--- src/components/user/LoginForm.jsx
+++ src/components/user/LoginForm.jsx
@@ -17,7 +17,7 @@ const LoginForm = () => {
     REACT_APP_KAKAO_API_KEY: KAKAO_API_KEY,
     REACT_APP_KAKAO_REDIRECT_URI: KAKAO_REDIRECT_URI,
   } = process.env;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}/redirect&response_type=code`;
 
   const dispatch = useDispatch();
   const queryClient = useQueryClient();
